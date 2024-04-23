4f77d465b5382a63bfbc8db4ba2ae48713cbb47c
Ankitluhar <ankitluhar5121@gmail.com>
2024-04-23 18:15:59 +0530

    third commit

--- index.cjs
+++ index.cjs
@@ -1,10 +1,11 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const blogs = require("./api/blogsData.json");
 const port = process.env.PORT || 5000;
 
-// middleware
+const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -13,16 +14,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/blogs", (req, res) => {
-  res.send(blogs);
+  res.json(blogs); // Return JSON instead of sending directly
 });
+
 app.get("/blogs/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  // console.log(id)
-  const blog = blogs.filter((b) => b.id === id);
-  // console.log(blog)
-  res.send(blog);
+  const blog = blogs.find((b) => b.id === id); // Use find instead of filter for single item
+  if (blog) {
+    res.json(blog); // Return JSON instead of sending directly
+  } else {
+    res.status(404).send("Blog not found"); // Send 404 status if blog is not found
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
