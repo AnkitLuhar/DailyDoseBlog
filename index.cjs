de09da889d1197f2b3ba7163cbf9cffc85e85f84
Ankitluhar <ankitluhar5121@gmail.com>
2024-04-23 19:00:39 +0530

    s commit

--- index.cjs
+++ index.cjs
@@ -1,37 +1,29 @@
 const express = require("express");
-const serverless = require("serverless-http");
+const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const blogs = require("./api/blogsData.json");
-const port = process.env.PORT || 5000;
-const router = express.Router();
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Blog server is running!");
 });
 
-router.get("/blogs", (req, res) => {
-  res.json(blogs); // Return JSON instead of sending directly
+app.get("/blogs", (req, res) => {
+  res.send(blogs);
 });
-
-router.get("/blogs/:id", (req, res) => {
+app.get("/blogs/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const blog = blogs.find((b) => b.id === id); // Use find instead of filter for single item
-  if (blog) {
-    res.json(blog); // Return JSON instead of sending directly
-  } else {
-    res.status(404).send("Blog not found"); // Send 404 status if blog is not found
-  }
+  // console.log(id)
+  const blog = blogs.filter((b) => b.id === id);
+  // console.log(blog)
+  res.send(blog);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-
-app.use("/api/blogsData", router);
-module.exports.handler = serverless(app);
